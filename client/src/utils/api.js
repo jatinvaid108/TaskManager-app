a2d38e517c91191b8fcb37fa0a04ee5fb5963e67
Jatin <jatinvaid00540@gmail.com>
2025-12-01 19:02:57 +0530

    Deploying

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "https://taskmanager-app-ae03.onrender.com/api",
-  withCredentials: true, // important for sending JWT cookie
+  baseURL:
+    import.meta.env.VITE_API_BASE_URL ||
+    "http://localhost:5000/api",  // local fallback
+  withCredentials: true,
 });
 
 export default api;
